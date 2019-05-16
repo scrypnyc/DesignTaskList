0e2e07d6f50bffefc416f3993a0e066b08affd83
scrypnyc <alexanderlookua@gmail.com>
2019-05-16 14:10:41 +0300

    Initial Commit

---
+++ User-Design-Mail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  User-Design-Mail
+//
+//  Created by Alexander Skrypnyk on 5/16/19.
+//  Copyright © 2019 skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
