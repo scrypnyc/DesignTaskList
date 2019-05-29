4b29a499e3561df3f183fdb4d22db8a3b001fa6a
Alexander <alexanderlookua@gmail.com>
2019-05-29 22:44:59 +0300

    Update: Task.swift

--- User-Design-Mail/Model/Task.swift
+++ User-Design-Mail/Model/Task.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Firebase
+
+struct Task {
+    
+    let title: String
+    let userID: String
+    let ref: DatabaseReference?
+    var completed: Bool = false
+    
+    init(title: String, userID: String) {
+        self.title = title
+        self.userID = userID
+        self.ref = nil
+    }
+    
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        title = snapshotValue["title"] as! String
+        userID = snapshotValue["userID"] as! String
+        completed = snapshotValue["completed"] as! Bool
+        ref = snapshot.ref
+    }
+}
